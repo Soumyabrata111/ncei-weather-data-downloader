8784e945201c935511c17e99e24ce0d1118209d9
Soumyabrata Bhattacharjee <soumyabrata111@gmail.com>
2023-03-30 23:13:12 +0530

    Update hourly_wind_data.py

--- hourly_wind_data.py
+++ hourly_wind_data.py
@@ -1,27 +1,37 @@
 import pandas as pd
+import os
 
-# Read the data and keep only the relevant columns
-df = pd.read_csv('43318099999.csv', usecols=['STATION', 'DATE', 'LATITUDE', 'LONGITUDE', 'ELEVATION', 'NAME', 'REPORT_TYPE', 'WND'])
+def process_csv(file):
+    df = pd.read_csv(file, usecols=['STATION', 'DATE', 'LATITUDE', 'LONGITUDE', 'ELEVATION', 'NAME', 'REPORT_TYPE', 'WND'])
 
-# Store the station name
-station_name = df['NAME'][0]
+    station_name = df['NAME'][0]
 
-# Split the 'WND' column into relevant columns
-df[['Angle', 'Angle_Measurement_Quality', 'Wind_Obs_Character', 'Wind_Speed', 'Wind_Speed_Quality']] = df['WND'].str.split(",", expand=True)
+    df[['Angle', 'Angle_Measurement_Quality', 'Wind_Obs_Character', 'Wind_Speed', 'Wind_Speed_Quality']] = df['WND'].str.split(",", expand=True)
 
-# Convert data types
-df = df.astype({'Angle': float, 'Angle_Measurement_Quality': float, 'Wind_Obs_Character': str, 'Wind_Speed': float, 'Wind_Speed_Quality': float})
+    df = df.astype({'Angle': float, 'Angle_Measurement_Quality': float, 'Wind_Obs_Character': str, 'Wind_Speed': float, 'Wind_Speed_Quality': float})
 
-# Drop rows with faulty data
-df = df[(df['Angle'] != 999) & (df['Angle_Measurement_Quality'] == 1) & (df['Wind_Obs_Character'] == 'N') & (df['Wind_Speed'] != 9999) & (df['Wind_Speed_Quality'] == 1) & (df['REPORT_TYPE'] == 'FM-12')]
+    df = df[(df['Angle'] != 999) & (df['Angle_Measurement_Quality'] == 1) & (df['Wind_Obs_Character'] == 'N') & (df['Wind_Speed'] != 9999) & (df['Wind_Speed_Quality'] == 1) & (df['REPORT_TYPE'] == 'FM-12')]
 
-# Convert the DATE column to datetime format and create new columns for Year, Month, Day, Hour, Minutes, and Seconds
-df['DATE'] = pd.to_datetime(df['DATE'])
-df[['Year', 'Month', 'Day', 'Hour', 'Minutes', 'Seconds']] = df['DATE'].apply(lambda x: pd.Series([x.year, x.month, x.day, x.hour, x.minute, x.second]))
+    df['DATE'] = pd.to_datetime(df['DATE'])
+    df['Year'] = df['DATE'].dt.year
+    df['Month'] = df['DATE'].dt.month
+    df['Day'] = df['DATE'].dt.day
+    df['Hour'] = df['DATE'].dt.hour
+    df['Minutes'] = df['DATE'].dt.minute
+    df['Seconds'] = df['DATE'].dt.second
 
-# Keep only the relevant columns and filter rows with Minutes and Seconds equal to 0
-df = df[['Year', 'Month', 'Day', 'REPORT_TYPE', 'Wind_Speed', 'Angle', 'Hour', 'Minutes', 'Seconds']]
-df = df[(df['Minutes'] == 0) & (df['Seconds'] == 0)]
+    df = df[['Year', 'Month', 'Day', 'REPORT_TYPE', 'Wind_Speed', 'Angle', 'Hour', 'Minutes', 'Seconds']]
+    df = df[(df['Minutes'] == 0) & (df['Seconds'] == 0)]
 
-# Save the modified dataframe to a new csv file named 'Modified_station_name.csv'
-df.to_csv(f'Modified_{station_name}.csv', index=False)
+    output_file = f'Modified_{station_name}.csv'
+    df.to_csv(output_file, index=False)
+    print(f'Saved modified data to {output_file}')
+
+# Folder containing the CSV files
+folder_path = '/path/to/csv/files'
+
+# Iterate over all CSV files in the folder
+for file_name in os.listdir(folder_path):
+    if file_name.endswith('.csv') and file_name[0].isdigit():
+        file_path = os.path.join(folder_path, file_name)
+        process_csv(file_path)
